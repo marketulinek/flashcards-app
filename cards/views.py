87da478fb1f2d6d2b91ba20e83a8d0da4583446d
Marketa <habranova.marketa@gmail.com>
2022-07-27 21:44:21 +0200

    Verification of the answer

--- cards/views.py
+++ cards/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import ListView, CreateView, UpdateView
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, redirect
 from .models import Card
+from .forms import CardCheckForm
 import random
 
 
@@ -18,10 +20,11 @@ class CardUpdateView(CardCreateView, UpdateView):
 
 class BoxView(CardListView):
     template_name = 'cards/box.html'
+    form_class = CardCheckForm
 
     def get_queryset(self):
         return Card.objects.filter(box=self.kwargs['box_num'])
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['box_number'] = self.kwargs['box_num']
@@ -30,3 +33,12 @@ class BoxView(CardListView):
             context['check_card'] = random.choice(self.object_list)
 
         return context
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            card = get_object_or_404(Card, id=form.cleaned_data['card_id'])
+            card.move(form.cleaned_data['solved'])
+
+        # Redirect the request to the same page from which the request was posted
+        return redirect(request.META.get('HTTP_REFERER'))
